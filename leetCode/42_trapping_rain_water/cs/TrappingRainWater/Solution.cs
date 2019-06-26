d21df10cd6048dc6afabc8f52b5473c91cc9fe81
Chad Queen <chaddolan82@gmail.com>
2019-06-26 11:05:37 -0400

    LeetCode 42 Trapping Rain Water

---
+++ leetCode/42_trapping_rain_water/cs/TrappingRainWater/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TrappingRainWater
+{
+    public class Solution
+    {
+        public int Trap(int[] height)
+        {
+            if (height.Length == 0)
+            {
+                return 0;
+            }
+
+            var trappedRainCellCount = 0;
+            var currentIndex = 0;
+
+            while (currentIndex < height.Length - 1)
+            {
+                if (height[currentIndex] > height[currentIndex + 1])
+                {
+                    var closingTrapIndex = currentIndex + 1;
+                    var trapBottomTotal = 0;
+
+                    while (closingTrapIndex < height.Length && height[currentIndex] > height[closingTrapIndex])
+                    {
+                        trapBottomTotal += height[closingTrapIndex];
+                        closingTrapIndex++;
+                    }
+
+                    if (closingTrapIndex == height.Length || height[closingTrapIndex] < height[currentIndex])
+                    {
+                        height[currentIndex]--;
+                        continue;
+                    }
+
+                    if (closingTrapIndex - currentIndex > 1)
+                    {
+                        trappedRainCellCount += (Math.Min(height[currentIndex], height[closingTrapIndex]) * (closingTrapIndex - currentIndex - 1)) - trapBottomTotal;
+                    }                
+
+                    currentIndex = closingTrapIndex;
+                }
+                else
+                {
+                    currentIndex++;
+                }
+            }
+
+            return trappedRainCellCount;
+        }
+    }
+}
